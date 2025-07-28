9ce8c67fa3c1d070ad09096898e471e4696cc3b1
mandyval17 <mayurvalecha2603@gmail.com>
2025-07-28 16:05:54 +0530

    Update BACKEND_URL to production endpoint and comment out local development URL

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = 'https://timeevents.onrender.com/api/v1';
-export const BACKEND_URL = 'http://localhost:5000/api/v1';
+export const BACKEND_URL = 'https://timeevents.onrender.com/api/v1';
+// export const BACKEND_URL = 'http://localhost:5000/api/v1';
